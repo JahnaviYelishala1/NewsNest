055d07559953fc08ce82d40130e8278e570a9fcc
JahnaviYelishala1 <yelishalajahnavi2020@gmail.com>
2025-06-08 18:47:08 +0530

    Move React app to client folder and add server.js backend

---
+++ client/src/components/News.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+export class News extends Component {
+  static defaultProps = {
+    country: 'us',
+    pageSize: 8,        
+    category: 'business'
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0
+    };
+  }
+
+  async componentDidMount() {
+    this.props.setProgress(10);
+    this.setState({ loading: true });
+    const newsUrl = `/api/news?country=${this.props.country}&category=${this.props.category}&page=1&pageSize=${this.props.pageSize}`;
+
+    let data = await fetch(newsUrl);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles || [],
+      totalResults: parsedData.totalResults || 0,
+      loading: false
+    });
+    document.title = `${this.props.category} - NewsNest`;
+    this.props.setProgress(100);
+  }
+
+  fetchMoreData = async () => {
+  const nextPage = this.state.page + 1;
+  const url = `/api/news?country=${this.props.country}&category=${this.props.category}&page=${nextPage}&pageSize=${this.props.pageSize}`;
+  
+  let data = await fetch(url);
+  let parsedData = await data.json();
+
+  this.setState({
+    page: nextPage,
+    articles: this.state.articles.concat(parsedData.articles || []),
+    totalResults: parsedData.totalResults || this.state.totalResults,
+  });
+}
+
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center my-3">Today's - Top {this.props.category} Headlines</h1>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className="row">
+            {this.state.articles.map((element) => (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ''}
+                  description={element.description ? element.description.slice(0, 88) : ''}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            ))}
+          </div>
+        </InfiniteScroll>
+      </div>
+    );
+  }
+}
+
+export default News;
+
