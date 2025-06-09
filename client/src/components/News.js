c9bcc8bb7a30ead8a977e26953a02d0c4b7fe588
JahnaviYelishala1 <yelishalajahnavi2020@gmail.com>
2025-06-09 20:18:44 +0530

    changing News.js fetching method

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -29,7 +29,7 @@ export class News extends Component {
   async componentDidMount() {
     this.props.setProgress(10);
     this.setState({ loading: true });
-    const newsUrl = `/api/news?country=${this.props.country}&category=${this.props.category}&page=1&pageSize=${this.props.pageSize}`;
+     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=1&pageSize=${this.props.pageSize}`;
 
     let data = await fetch(newsUrl);
     let parsedData = await data.json();
@@ -44,8 +44,8 @@ export class News extends Component {
 
   fetchMoreData = async () => {
   const nextPage = this.state.page + 1;
-  const url = `/api/news?country=${this.props.country}&category=${this.props.category}&page=${nextPage}&pageSize=${this.props.pageSize}`;
-  
+   const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${nextPage}&pageSize=${this.props.pageSize}`;
+
   let data = await fetch(url);
   let parsedData = await data.json();
 
